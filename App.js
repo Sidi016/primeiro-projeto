c19cd3cba4344214efb1aba0226747ae25645887
Sidi016 <sidimir.souza@etec.sp.gov.br>
2024-02-22 10:42:33 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Sidimir Henrique de Paula Souza</Text>
+      <Text>Desenvolvimento de dispositivos móveis 22/02/2024</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
